8d19e607185368ee62859379fd415cf9e440d377
Rupesh Yogi <rupeshyogi712@gmail.com>
2025-04-11 23:41:47 +0100

    added animation to the text

--- js/script.js
+++ js/script.js
@@ -49,15 +49,7 @@ $(document).ready(function() {
 // });
 
 
-// document.getElementById('toggleButton').addEventListener('click', function() {
-//     document.body.classList.toggle('dark-mode');
-
-//     if (document.body.classList.contains('dark-mode')) {
-//         document.body.style.backgroundColor = '#121212';
-//     } else {
-//         document.body.style.backgroundColor = 'white';
-//     }
-// });
+// 
 
 
 $('#toggleButton').click(function() {
