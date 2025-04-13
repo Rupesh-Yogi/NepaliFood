de60a6455a3d9c7f06ef18c2e385bff88d451b6d
Rupesh Yogi <rupeshyogi712@gmail.com>
2025-04-14 00:00:10 +0100

    changing cleaned all the code of every files

--- js/script.js
+++ js/script.js
@@ -34,31 +34,3 @@ $(document).ready(function() {
     });
 });
 
-
-// const toggleThemeBtn = document.getElementById("toggleTheme");
-// toggleThemeBtn.addEventListener("click", () => {
-//   document.body.classList.toggle("dark-mode");
-// });
-
-
-// // Get the toggle button
-// const toggleButton = document.getElementById('toggleButton');
-
-// // Add click event to toggle dark mode
-// toggleButton.addEventListener('click', () => {
-//     document.body.classList.toggle('dark-mode');
-// });
-
-
-// 
-
-
-$('#toggleButton').click(function() {
-    $('body').toggleClass('dark-mode');
-    
-    if ($('body').hasClass('dark-mode')) {
-        $('body').css('background-color', '#121212');
-    } else {
-        $('body').css('background-color', 'white');
-    }
-});
